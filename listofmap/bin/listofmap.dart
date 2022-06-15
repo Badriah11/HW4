42244dc0a309d0540649ec3e89c566f1cfbf8398
Badriah11 <107368403+Badriah11@users.noreply.github.com>
2022-06-15 17:24:29 +0300

    Add files via upload
    Standard Input in Dart (stdin):
    The standard input stream reads data both synchronously and asynchronously from the keyboard. 
    
    In Dart programming language, .readLineSync( ) function is used to accept input from the user via console. The definition of this function is stored in the ‘dart:io’ library; therefore, to use its functions and objects, you need to import this library in your code. If you fail to do so, the compiler will report an error, and the program will not execute.

---
+++ listofmap/bin/listofmap.dart
@@ -0,0 +1,13 @@
+
+var color1="blue";
+var color2="red";
+var color3="yllow";
+var color4="green";
+var color5="orang";
+List<Map<String,String>> frouit <String,String?>=[{color1:"blue",color2:"red",color3:"yllow",color4:"green",color5:"orang",null}]
+;
+
+void main(List<String> args) {
+  
+}
+ 
